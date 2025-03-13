7b727442c8dc71512cf100f4b0e8dbefecb0900f
dictz <vgviktorovna@gmail.com>
2025-03-13 13:46:01 +0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+'use strict';
+const CACHE_NAME = 'dictz_v2';
+try {
+	self.addEventListener('install', function(event) {
+		try {
+			event.waitUntil((async function() {
+				try {
+					const cache = await caches.open(CACHE_NAME);
+					
+					
+					cache.addAll(['/index.html']);
+					
+					
+				} catch (er) {}
+			})());
+		} catch (er) {}
+	});
+	self.addEventListener('fetch', function(event) {
+		try {
+			event.respondWith((async function() {
+				try {
+					const cache = await caches.open(CACHE_NAME);
+					const cachedResponse = await cache.match(event.request);
+					if(cachedResponse) return cachedResponse;
+					else {
+						const fetchResponse = await fetch(event.request);
+						cache.put(event.request, fetchResponse.clone());
+						return fetchResponse;
+					}
+				} catch (er) {}
+			})());
+		} catch (er) {}
+	});
+} catch (er) {}
